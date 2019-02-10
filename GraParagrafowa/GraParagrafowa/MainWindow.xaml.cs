8c17ee57dbf549464fd2c282b94bdf598a349055
maxmruk <47156895+maxmruk@users.noreply.github.com>
2019-02-10 23:05:35 +0100

    Add files via upload
    Wstępny projekt wyglądu interfejsu z przyciskami

---
+++ GraParagrafowa/GraParagrafowa/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraParagrafowa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenWindow(object sender, RoutedEventArgs e)
+        {
+            OpenWindow objSecondWindow = new SecondWindow();
+            this.Visibility = Visibility.Hidden;
+            objGraParagrafowa._2.show
+        }
+
+        private void Open(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GraParagrafowa/GraParagrafowa/SecondWindow1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GraParagrafowa
+{
+    /// <summary>
+    /// Interaction logic for SecondWindow1.xaml
+    /// </summary>
+    public partial class SecondWindow1 : Window
+    {
+        public SecondWindow1()
+        {
+            InitializeComponent();
+        }
+    }
+}
